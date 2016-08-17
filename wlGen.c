f32457fa1124dcaa7b9566b873ecd28ba8b63155
yash <yashgulati.g1@gmail.com>
2016-08-17 12:13:32 +0530

    creating and filling the files

--- wlGen.c
+++ wlGen.c
@@ -5,20 +5,25 @@
 int main(){
     int i;
     char fileName[30];
-    FILE *fp;
+    FILE *fNames,*fCurrent,command[100];
     char *line = NULL;
     size_t len = 0;
-    size_t read;
+    //size_t read;
     const size_t line_size = 300;
     line = malloc(line_size);
     if( access( "fileNames.lst", F_OK ) == -1 )
-        system("crunch 3 3 abcdefghijklmnopqrstuvwxyz -o fileNames.lst");
+        system("crunch 3 3 abcd -o fileNames.lst");
     else printf("fileNames.lst already exists.\n");
-    fp = fopen("./fileNames.lst","r");
+    fNames = fopen("./fileNames.lst","r");
 
-while (fgets(line, line_size, fp) != NULL)  {
-    printf("%s",line);
-    system("read -r -p \"Press space to continue...\" key");
+    while (fgets(line, line_size, fNames) != NULL)  {
+        printf("Making file %s",line);
+        fCurrent = fopen(line,"w");
+        strcpy(command,strcat(strcat("crunch 8 8 abcd -t ",line),"@@@@@"));
+        system(command);
+        fclose(fCurrent);
+        //system("read -r -p \"Press space to continue...\" key");
 }
+
     return 0;
 }
