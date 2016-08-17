3f8c8480e1a66b11f05af11d13febf9db626959e
yash <yashgulati.g1@gmail.com>
2016-08-17 11:26:46 +0530

    still errors

--- wlGen.c
+++ wlGen.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
+#include<conio.h>
 #include<unistd.h>
+#include<stdlib.h>
 int main(){
     char fileName[30];
     FILE *fp;
     char *line = NULL;
     size_t len = 0;
     size_t read;
+    const size_t line_size = 300;
+    char* line = malloc(line_size);
     if( access( "fileNames.lst", F_OK ) == -1 )
         system("crunch 3 3 abcdefghijklmnopqrstuvwxyz -o fileNames.lst");
     else printf("fileNames.lst already exists.\n");
-    fp = fopen("fileNames.lst",'r');
-    while ((read = getline(&line, &len, fp)) != -1) {
-        printf("Retrieved line of length %zu :\n", read);
-        printf("%s", line);
-        getch();
-    }
+    fp = fopen("./fileNames.lst",'r');
+
+while (fgets(line, line_size, fp) != NULL)  {
+    printf(line);
+}
     return 0;
 }
