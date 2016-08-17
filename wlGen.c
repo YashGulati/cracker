2506fdc5ef59d2ea4112fff9ab619a176c95fe17
yash <yashgulati.g1@gmail.com>
2016-08-17 11:17:56 +0530

    working with errors

--- wlGen.c
+++ wlGen.c
@@ -3,13 +3,13 @@
 int main(){
     char fileName[30];
     FILE *fp;
-    char * line = NULL;
+    char *line = NULL;
     size_t len = 0;
-    ssize_t read;
+    size_t read;
     if( access( "fileNames.lst", F_OK ) == -1 )
         system("crunch 3 3 abcdefghijklmnopqrstuvwxyz -o fileNames.lst");
     else printf("fileNames.lst already exists.\n");
-    *fp = fopen("fileNames.lst",'r');
+    fp = fopen("fileNames.lst",'r');
     while ((read = getline(&line, &len, fp)) != -1) {
         printf("Retrieved line of length %zu :\n", read);
         printf("%s", line);
