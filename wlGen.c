0c3b484a01e515d5f1cd18cb76ca94855e720dba
yash <yashgulati.g1@gmail.com>
2016-08-17 11:11:06 +0530

    tried to read line by line of firstNames.lst

--- wlGen.c
+++ wlGen.c
@@ -2,10 +2,18 @@
 #include<unistd.h>
 int main(){
     char fileName[30];
+    FILE *fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
     if( access( "fileNames.lst", F_OK ) == -1 )
         system("crunch 3 3 abcdefghijklmnopqrstuvwxyz -o fileNames.lst");
-    else printf("fileNames.lst already exists.");
-
-
+    else printf("fileNames.lst already exists.\n");
+    *fp = fopen("fileNames.lst",'r');
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu :\n", read);
+        printf("%s", line);
+        getch();
+    }
     return 0;
 }
