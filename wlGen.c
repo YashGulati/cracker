d506ed944a5d1db48a1ee4a9152b92c4a8fe282f
yash <yashgulati.g1@gmail.com>
2016-08-17 10:52:54 +0530

    prints the already exists message

--- wlGen.c
+++ wlGen.c
@@ -2,9 +2,10 @@
 #include<unistd.h>
 int main(){
     char fileName[30];
-    if( access( "fileNames.lst", F_OK ) == -1 )
+    if( access( "fileNames.lst", F_OK ) == -1 ){
         system("crunch 3 3 abcdefghijklmnopqrstuvwxyz -o fileNames.lst");
-
+        printf("fileNames.lst already exists.");
+    }
 
 
 
