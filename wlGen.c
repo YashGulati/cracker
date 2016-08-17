b92f5c6b2e99e48a66fe0973c59a72597d30d3d6
yash <yashgulati.g1@gmail.com>
2016-08-17 10:50:45 +0530

    checks whether fileNames.lst already exists

--- wlGen.c
+++ wlGen.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include<unistd.h>
 int main(){
     char fileName[30];
+    if( access( "fileNames.lst", F_OK ) == -1 )
+        system("crunch 3 3 abcdefghijklmnopqrstuvwxyz -o fileNames.lst");
 
-    system("crunch 3 3 abcdefghijklmnopqrstuvwxyz -o fileNames.lst");
+
 
 
     return 0;
