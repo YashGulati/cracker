78fc8151cc5bf592e0b20f710d3a314db0018df5
yash <yashgulati.g1@gmail.com>
2016-08-25 12:59:17 +0530

    made the printer() (Working), now the printer() and wordCount() are independent of length and strlen(charset)

--- firstwl3.c
+++ firstwl3.c
@@ -2,7 +2,7 @@
 #include<string.h>
 #include<math.h>
 char charset[] = "abcd";
-int length = 4;
+int length = 3;
 int wc=0;
 int wordCount(charlen,wordlen){
     if(wordlen == 1) return strlen(charset);
@@ -12,9 +12,16 @@ int main(){
     int i,total_words, dbg=0;
     int pos[10],flag[10];
     int charset_length = strlen(charset);
+    void printer(){
+        int p;
+        for(p=length;p>=2;p--)
+            if(flag[length-p]!= -1) printf("%c",charset[pos[length-p]]);
+        printf("%c\n",charset[pos[length-1]++]);
+    }
     memset(pos,-1,sizeof(pos));
     memset(pos,0,length*4);
     memset(flag,-1,sizeof(flag));
+
     total_words = wordCount(charset_length,length);
     for(i=0;i<total_words;i++){
         if(dbg==1) printf("%d: ",i);
@@ -53,10 +60,7 @@ int main(){
                 pos[length-4] = 0;
             }
         }
-        if(flag[length-4]!= -1) printf("%c",charset[pos[length-4]]);
-        if(flag[length-3]!= -1) printf("%c",charset[pos[length-3]]);
-        if(flag[length-2]!= -1) printf("%c",charset[pos[length-2]]);
-        printf("%c\n",charset[pos[length-1]++]);
+        printer();
     }
     return 0;
 }
