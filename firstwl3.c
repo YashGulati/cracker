58b94eecc26198dfa7cb927608ce634c4c69aa09
yash <yashgulati.g1@gmail.com>
2016-09-02 22:20:28 +0530

    Done with Project

--- firstwl3.c
+++ firstwl3.c
@@ -2,14 +2,14 @@
 #include<string.h>
 #include<math.h>
 char charset[] = "abcd";
-int length = 3;
+int length = 8;
 int wc=0;
 int wordCount(charlen,wordlen){
     if(wordlen == 1) return strlen(charset);
     return pow(strlen(charset),wordlen) + wordCount(charlen,wordlen-1);
 }
 int main(){
-    int i,total_words, dbg=0;
+    int i,total_words, dbg=1;
     int pos[10],flag[10];
     int charset_length = strlen(charset);
     void printer(){
@@ -19,6 +19,7 @@ int main(){
         printf("%c\n",charset[pos[length-1]++]);
     }
     void ifer(){
+        int i;
      if( length>=1 ){ // if#1
             if(pos[length-1] == charset_length ){
                 pos[length-1] = 0;
@@ -28,36 +29,18 @@ int main(){
                 }
             }
          }
-         if( length>=2 && flag[length-2]!= -1 ){ // if#2
-            flag[length-2] = 2;
-            if(pos[length-2] == charset_length ){
-                pos[length-2] = 0;
-                if(pos[length-3] != -1 ){
-                    if(flag[length-3] ==2 ) pos[length-3]++;
-                    flag[length-3] = 1;
+        for(i=2;i<=length;i++){
+        if( length>=i && flag[length-i]!= -1 ){ // if#i
+            flag[length-i] = 2;
+            if(pos[length-i] == charset_length ){
+                pos[length-i] = 0;
+                if(pos[length-(i+1)] != -1 ){
+                    if(flag[length-(i+1)] ==2 ) pos[length-(i+1)]++;
+                    flag[length-(i+1)] = 1;
                 }
             }
         }
-        if( length>=3 && flag[length-3]!= -1 ){ // if#3
-            flag[length-3] = 2;
-            if(pos[length-3] == charset_length ){
-                pos[length-3] = 0;
-                if(pos[length-4] != -1 ){
-                    if(flag[length-4] ==2 ) pos[length-4]++;
-                    flag[length-4] = 1;
-                }
-            }
-        }
-        if( length>=4 && flag[length-4]!= -1 ){ // if#4
-            flag[length-4] = 2;
-            if(pos[length-4] == charset_length ){
-                pos[length-4] = 0;
-                if(pos[length-5] != -1 ){
-                    if(flag[length-5] ==2 ) pos[length-5]++;
-                    flag[length-5] = 1;
-                }
-            }
-        }
+    }
     }
     memset(pos,-1,sizeof(pos));
     memset(pos,0,length*4);
@@ -67,6 +50,7 @@ int main(){
         if(dbg==1) printf("%d: ",i);
         ifer();
         printer();
+        if(dbg==1 && i%28==0) getch();
     }
     return 0;
 }
